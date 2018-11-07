15239c4bb28dd5cfc1f1477b20864684d77095fd
username <holy09south16a@gmail.com>
2018-11-08 06:20:38 +0900

    Update page

--- カメラ/index.js
+++ カメラ/index.js
@@ -29,6 +29,29 @@ function draw() {
     requestAnimationFrame(draw);
 }
 
-function take_picture(){
-    alert("press button2");
-}
+function take_picture() {
+    alert("press button4");
+
+    context.drawImage(video, 0, 0);
+    const url = canvas.toDataURL("image/png");
+    Tesseract
+        .recognize(url, {
+            lang: 'jpn'
+        })
+        .progress(function (p) {
+            $("#msg").text(p.status + ": " + p.progress)
+        })
+        .then(function (result) {
+            console.log(result.symbols);
+            $("#msg").text("Progress Complete")
+            var elem = document.createElement('div');
+            elem.innerHTML = "<div id=" + num + " style='width:300px; background-color:#EEEEEE'><img src=" + url + " /></div><br>"
+            var parent = document.getElementById("results");
+            parent.insertBefore(elem, parent.firstChild);
+            var numdiv = document.getElementById(num);
+            var msg = document.createElement('div');
+            msg.innerHTML = result.text;
+            numdiv.appendChild(msg);
+            num += 1;
+        });
+};
